115cbe84418ceecedf476981445be6f1bf172224
nezox1488 <maximnasarenko@gmail.com>
2026-04-26 23:04:41 +0300

    Introduce Renderer2D pipeline with backend, batcher and transform stack

---
+++ src/main/java/kefirdlc/module/BasicModule.java
@@ -0,0 +1,9 @@
+package kefirdlc.module;
+
+// coded by sitoku \\
+
+public class BasicModule extends Module {
+    public BasicModule(String name, ModuleCategory category) {
+        super(name, category);
+    }
+}
